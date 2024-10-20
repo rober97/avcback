81b51a2d1b589283adcfccbab0742d759cdbec40
rober <roberastorgab@gmail.com>
2024-10-20 18:42:19 -0300

    asddsaddsasd

--- src/controllers/minecraft.js
+++ src/controllers/minecraft.js
@@ -1,20 +1,32 @@
 const net = require('net');
 
-const MINECRAFT_SERVER_HOST = '209.192.187.106';  // La dirección IP de tu servidor de Minecraft
-const MINECRAFT_SERVER_PORT = 8029;  // Asegúrate de cambiar esto al puerto que esté escuchando tu plugin de Spigot
+const MINECRAFT_SERVER_HOST = '209.192.187.106'; // La dirección IP de tu servidor de Minecraft
+const MINECRAFT_SERVER_PORT = 8029; // El puerto que esté escuchando tu plugin de Spigot
 
-const clientSocket = new net.Socket();
+let clientSocket = new net.Socket();
 
-clientSocket.connect(MINECRAFT_SERVER_PORT, MINECRAFT_SERVER_HOST, function () {
-    console.log('Conectado al servidor de Minecraft!');
-});
+const connectToMinecraftServer = () => {
+  clientSocket.connect(MINECRAFT_SERVER_PORT, MINECRAFT_SERVER_HOST, function () {
+    console.log('\x1b[32m%s\x1b[0m', 'Conectado al servidor de Minecraft!');
+  });
 
-clientSocket.on('data', function (data) {
-    console.log('Recibido: ' + data);
-});
+  clientSocket.on('data', function (data) {
+    console.log('\x1b[36m%s\x1b[0m', 'Recibido: ' + data);
+  });
 
-clientSocket.on('close', function () {
-    console.log('Conexión cerrada');
-});
+  clientSocket.on('close', function () {
+    console.log('\x1b[31m%s\x1b[0m', 'Conexión cerrada, intentando reconectar...');
+    setTimeout(connectToMinecraftServer, 5000); // Intentar reconectar después de 5 segundos
+  });
+
+  clientSocket.on('error', function (err) {
+    console.error('\x1b[31m%s\x1b[0m', 'Error en la conexión:', err.message);
+    clientSocket.destroy(); // Destruir el socket en caso de error
+    setTimeout(connectToMinecraftServer, 5000); // Intentar reconectar después de 5 segundos
+  });
+};
+
+// Iniciar la conexión al servidor de Minecraft
+connectToMinecraftServer();
 
 module.exports = clientSocket;
